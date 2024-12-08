e3189925fdaaf694df92707b1f027112f9d23522
henriqvedev <dtrr2k21@gmail.com>
2024-12-08 19:22:42 -0300

    upando anatomia2

---
+++ src/edu/SintaxeJava/Anatomiadasclasses/Anatomia2.java
@@ -0,0 +1,16 @@
+package edu.SintaxeJava.Anatomiadasclasses;
+
+public class Anatomia2 {
+
+    public static void main (String [] args) {
+
+        int ano = 2021;
+
+        ano = 2022;
+
+        final String BR = "Brasil";
+
+
+        //     BR = "Brazil";    daria erro! variavel com letra maiuscula e final e nao pode sofrer mais alterações no valor
+    }
+}
