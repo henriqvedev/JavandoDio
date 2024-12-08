bcd5e138336ec4a0db7d2b5fbad6dcd0ca58d63a
henriqvedev <dtrr2k21@gmail.com>
2024-12-08 15:50:54 -0300

    upando anatomia das classes

---
+++ src/edu/SintaxeJava/Anatomiadasclasses/Anatomia1.java
@@ -0,0 +1,9 @@
+package edu.SintaxeJava.Anatomiadasclasses;
+
+public class Anatomia1{
+
+    public static void main (String [] args) {
+
+        System.out.println("eai familia tudo na paz?");
+    }
+}
