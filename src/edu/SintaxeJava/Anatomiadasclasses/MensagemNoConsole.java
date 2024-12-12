f6a8abb82a76d4ffc02446424e0c19a0d16a4b5c
henriqvedev <dtrr2k21@gmail.com>
2024-12-12 07:00:47 -0300

    Removendo arquivos .java da branch main

--- src/edu/SintaxeJava/Anatomiadasclasses/MensagemNoConsole.java
+++
@@ -1,9 +0,0 @@
-package edu.SintaxeJava.Anatomiadasclasses;
-
-public class MensagemNoConsole{
-
-    public static void main (String [] args) {
-
-        System.out.println("eai familia tudo na paz?");
-    }
-}
