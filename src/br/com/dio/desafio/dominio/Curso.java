dbf239691c5f068938e0ed82eda93b5bfad2e26f
henriqvedev <dtrr2k21@gmail.com>
2025-04-01 06:20:25 -0300

    camilaPoo

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,44 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(final String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(final String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return this.cargaHoraria;
+    }
+
+    public void setCargaHoraria(final int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
